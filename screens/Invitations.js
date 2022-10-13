1c9bf61c3d6463509178d0744ebde1a47bd91289
Nickolinhas-rei-delas <cabocanick@gmail.com>
2022-10-12 22:11:33 -0300

    iniciando a Invitations, caso eseja dando erro, excluir a function do useEffect

--- screens/Invitations.js
+++ screens/Invitations.js
@@ -1,6 +1,23 @@
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-
+import { useEffect } from 'react';
 const Invitations = () =>{
+    useEffect(() => {
+        const user  = Firebase.auth().currentUser.uid
+        let ref = Firebase.firestore().collection('user').where("user_id", "==", user).onSnapshot(query =>{
+            const data   = []
+            query.forEach(doc =>{
+                data.push({
+                    ...doc.data(),
+                    key: doc.id
+                })
+            })
+          
+        })
+        return () => ref()
+    }, [])
+  
+  
+  
     return(
 
         <View>
